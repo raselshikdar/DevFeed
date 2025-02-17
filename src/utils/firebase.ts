6e41f45d57d4ef3abed68000432856eba13c7609
Rasel Shikdar <imrasel597@gmail.com>
2025-02-18 02:08:39 +0600

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,18 +4,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.REACT_FIRE_API_KEY,
+  authDomain: process.env.REACT_FIRE_AUTH_DOMAIN,
+  projectId: process.env.REACT_FIRE_PROJECT_ID,
+  storageBucket: process.env.REACT_FIRE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_FIRE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_FIRE_APP_ID,
 };
 
-// Initialize Firebase only if it hasn't been initialized
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// Firebase services
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
