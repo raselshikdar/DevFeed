1c34ec8978a2637e3a0d548f29a7df7632b99624
Rasel Shikdar <imrasel597@gmail.com>
2025-02-18 01:39:54 +0600

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,1 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
+const firebaseConfig = {
+  // your firebase config here
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
