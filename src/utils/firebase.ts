15a71b2543113ecf49876d64043ee5b9a3478edd
Rasel Shikdar <imrasel597@gmail.com>
2025-02-18 11:15:42 +0600

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_FIRE_API_KEY,
-  authDomain: process.env.REACT_FIRE_AUTH_DOMAIN,
-  projectId: process.env.REACT_FIRE_PROJECT_ID,
-  storageBucket: process.env.REACT_FIRE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_FIRE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_FIRE_APP_ID,
+  apiKey: process.env.REACT_APP_FIRE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIRE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
